fbd7e0bf645ab55b8bf6a8a41dd06d07f65c1657
Dinesh Prajapati <dineshprj7@gmail.com>
2026-06-14 22:53:28 +0530

    hero again

--- src/Features/components/Hero.tsx
+++ src/Features/components/Hero.tsx
@@ -1,21 +1,25 @@
-import { ArrowRight, Calendar } from "lucide-react";
-
+import { ArrowRight, Calendar, PlayIcon } from "lucide-react";
+import styles from "@/layout/style/Navbar.module.css";
+import { Play } from "next/font/google";
 export default function Hero() {
   return (
     <div className="py-28 px-30">
         <div className="absolute inset-0 h-8 w-8 bg-grid" />
-      <div className="">
+      <div className=" flex flex-col items-center justify-center bg-amber-600">
 
-        <div className="inline-flex items-center gap-3 px-2 py-1 rounded-full border border-purple-400 text-sm bg-purple-100">
-          <span className="w-2 h-2 rounded-full bg-purple-600"></span>
-          <span>Now accepting Q3 2025 projects</span>
+        <div className="inline-flex items-center gap-1 px-2 py-1 rounded-full border border-purple-400  bg-purple-100">
+          <span className="w-1 h-1 rounded-full bg-pink-400"></span>
+          <span className={`text-center ${styles.logot} text-purple-600 text-xs `}>Now accepting Q3 2025 projects</span>
         </div>
-        <h1 className="text-center">
+    <h1 className={`text-center flex flex-col text-7xl py-4 font-medium uppercase ${styles.logo} 
+    tracking-[1.3]
+   
+    `}>
           we build  
-          <span>software</span>
+          <span className=" bg-linear-to-r from-fuchsia-500 via-purple-600 to-purple-500 bg-clip-text text-transparent ">software</span>
           that scales
         </h1>
-        <p className="text-center">
+        <p className={`text-center ${styles.fontFirst} `}>
           Vyzion Innovations crafts premium web, mobile, and SaaS products for
           ambitious founders and enterprises. From MVP to IPO-ready.
         </p>
@@ -41,6 +45,11 @@ export default function Hero() {
             <Calendar />
             Book a Free call
             <ArrowRight />
+          </button>
+
+          <button>
+         <PlayIcon />
+            View Our Work
           </button>
         </div>
       </div>
